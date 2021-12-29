c8de8a7e5ed80a60e7891ce75d6f1729c0d07935
agustin-silvera <96441003+agustin-silvera@users.noreply.github.com>
2021-12-30 00:46:07 +0100

    Update PersonaControlador.java

--- src/main/java/com/example/demo/controlador/PersonaControlador.java
+++ src/main/java/com/example/demo/controlador/PersonaControlador.java
@@ -5,7 +5,6 @@ import com.example.demo.serv.PersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
 @RequestMapping("/persona")
 public class PersonaControlador {
